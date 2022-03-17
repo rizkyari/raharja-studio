88f6328627b479ddb7c640e51020b719a241d96b
rizkyari <rizkyari4121@gmail.com>
2022-03-17 14:31:04 +0700

    styling footer

--- src/pages/layouts/footer/_index.vue
+++ src/pages/layouts/footer/_index.vue
@@ -1,19 +1,52 @@
 <template>
-    <footer class="footer mt-auto">
+    <footer class="footer mt-auto pb-5">
         <div class="container-fluid">
             <div class="row">
-                <div class="footer-content col-md-6 col-lg-4">
-                    <div class="logo">
+                <div class="footer-content col-md-6 col-lg-4 text-start">
+                    <div class="logo mb-4">
                         <router-link to="/">
                             <img class='main-logo' src="@/assets/images/main-logo.png"/>
                         </router-link>
                     </div>
                     <div class="company-sum">
-                        <span>Raharja Studio </span>
-                        adalah tempat untuk kalian menyewa ruangan creative
+                        <span class="company-name">Raharja Studio </span>
+                        adalah tempat untuk kalian <br/>menyewa ruangan creative
                     </div>
-                    <div class="social-link">
-                        <img src='@/assets/icons/Instagram.png' class="ig-icons"/>
+                    <div class="social-link my-4">
+                        <img src='@/assets/icons/ig.png' class="ig-icons"/>
+                    </div>
+                    <div class="copyright">
+                        <span>©2022 Raharja Studio</span>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+
+                </div>
+                <div class="footer-content fw-bold col-md-6 col-lg-6 footer-wrapper">
+                    <div class="our-product">
+                        <div class="text-start ul-title">Product</div>
+                            <ul class="p-0 li-item">
+                                <li>Download</li>
+                                <li>Pricing</li>
+                                <li>Location</li>
+                            </ul>
+                    </div>
+                    <div class="our-product">
+                        <div class="text-start ul-title">Engage</div>
+                            <ul class="p-0 li-item">
+                                <li>FAQ</li>
+                                <li>Fasilitas</li>
+                                <li>About Us</li>
+                                <li>privacy Policy</li>
+                                <li>Terms of Service</li>
+                            </ul>
+                    </div>
+                    <div class="our-product">
+                        <div class="text-start ul-title">Earn Money</div>
+                            <ul class="p-0 li-item">
+                                <li>Affiliate</li>
+                                <li>Become Partner</li>
+                            </ul>
                     </div>
                 </div>
             </div>
